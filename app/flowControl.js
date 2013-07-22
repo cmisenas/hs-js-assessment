337b3470b209d8e2916d457367db671f87368cf6
cmisenas <acolyte.camille@gmail.com>
2013-07-22 16:02:18 -0400

    Passed flowControl.js file

--- app/flowControl.js
+++ app/flowControl.js
@@ -3,7 +3,15 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
+			if (num === undefined || isNaN(parseInt(num)))
+				return false;
+			var val = '';
+			if (num % 3 === 0)
+				val += 'fizz';
+			if (num % 5 === 0)
+				val += 'buzz';
+			return val === '' ? num : val;
+			// write a function that receives a number as its argument;
       // if the number is divisible by 3, the function should return 'fizz';
       // if the number is divisible by 5, the function should return 'buzz';
       // if the number is divisible by 3 and 5, the function should return
