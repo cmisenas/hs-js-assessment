05316d632a665612ff359d4be60cc939fe9d7709
cmisenas <acolyte.camille@gmail.com>
2013-07-22 17:03:51 -0400

    Passed functions.js

--- app/functions.js
+++ app/functions.js
@@ -3,39 +3,74 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+			return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+			return fn.call(obj);
     },
 
     functionFunction : function(str) {
-
+			return function(str2) {
+				return str + ', ' + str2;
+			}
     },
 
     makeClosures : function(arr, fn) {
-
+			var closure = [];
+			for (var i = 0; i < arr.length; i++) {
+				closure[i] = (function(arg) {
+					return function() {
+						return fn(arg)
+					};
+				}(arr[i]));
+			}
+			return closure;
     },
 
     partial : function(fn, str1, str2) {
-
+			return function(str3) {
+				return fn(str1, str2, str3);
+			}
     },
 
     useArguments : function() {
-
+			var val = 0;
+			for (var i = 0; i < arguments.length; i++) {
+				val += arguments[i];
+			}
+			return val;
     },
 
     callIt : function(fn) {
-
+			var args = [];
+			for (var i = 1; i < arguments.length; i++) {
+				args.push(arguments[i]);
+			}
+			fn.apply(null, args);
     },
 
     partialUsingArguments : function(fn) {
-
+			var args = [];
+			for (var i = 1; i < arguments.length; i++) {
+				args.push(arguments[i]);
+			}
+			return function() {
+				for (var i = 0; i < arguments.length; i++) {
+					args.push(arguments[i]);
+				}
+				return fn.apply(null, args);
+			}
     },
 
     curryIt : function(fn) {
-
-    }
+    	return function(a) {
+				return function(b) {
+					return function(c) {
+						return fn(a, b, c);
+					}
+				}
+			}
+		}
   };
 });
