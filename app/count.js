5865a8d246cdc3efc4bef33762e9c108258d7c52
cmisenas <acolyte.camille@gmail.com>
2013-07-24 11:33:19 -0400

    Passed count.js

--- app/count.js
+++ app/count.js
@@ -3,6 +3,18 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+			console.log(start);
+			var counter = setInterval(function() {
+				if (start < end) {
+					start++;
+					console.log(start);
+				}
+			}, 100);
+			return {
+				cancel: function() {
+					clearInterval(counter);
+				}
+			}
     }
   };
 });
