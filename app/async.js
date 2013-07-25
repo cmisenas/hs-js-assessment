1107c2f1d9fc1a9907b00fc29bb7b77dce3db7ec
cmisenas <acolyte.camille@gmail.com>
2013-07-25 09:39:48 -0400

    Passed async.js

--- app/async.js
+++ app/async.js
@@ -3,11 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
-    },
+			var jxhr = $.Deferred();
+			return jxhr.resolve(value);
+		},
 
     manipulateRemoteData : function(url) {
-
-    }
+			var data = [];
+			var jxhr = $.Deferred();
+			$.ajax(url).done(function(jsonObj) {
+				for (var i = 0; i < jsonObj.people.length; i++) {
+					data.push(jsonObj.people[i].name);
+				}
+				data.sort();
+				jxhr.resolve(data);
+			});
+			return jxhr.done(function() {
+				return this;
+			});
+		}
   };
 });
